29030798ea8f487dc476bc1b1139aa3cb29caa64
ghostrider-reborn <adithya.r02@outlook.com>
2021-08-22 16:37:18 +0800

    sm6225-common: Disable dexpreopt minidebuginfo
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Change-Id: Ief843fc411189906c5d0183a6ff4485f6b3041ee

--- common.mk
+++ common.mk
@@ -298,6 +298,7 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+WITH_DEXPREOPT_DEBUG_INFO := false
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
