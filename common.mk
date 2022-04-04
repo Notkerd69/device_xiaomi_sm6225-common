06169706c24be95e874ee1347cc50239e2271d67
LuK1337 <priv.luk@gmail.com>
2022-04-04 21:31:18 +0200

    sm6225-common: props: Disable remote keyguard animation
    Fixes slow post biometric auth screen wakeup.
    
    Change-Id: I5eef64a45fc7f3066e2ef2ada256f64b7154946a
    Signed-off-by: Notkerd69 <jerespn@gmail.com>

--- common.mk
+++ common.mk
@@ -325,6 +325,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Lawnchair \
     TrebuchetQuickStep
 
+# Keyguard
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.wm.enable_remote_keyguard_animation=0
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
