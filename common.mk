ac1e21895bca43b8a81ef95a42c92c763fe317f3
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-26 19:56:52 +0530

    sm6225-common: Set Brightness slider curve Implementation
    As per latest source Implementation
    
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- common.mk
+++ common.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti.recovery \
     android.hardware.boot@1.2-service
 
+# Brightness Curve
+PRODUCT_SYSTEM_PROPERTIES += \
+persist.sys.brightness.low.gamma=true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
